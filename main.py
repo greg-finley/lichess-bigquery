350922bdce8d1167dae22aca08d0608114f8b258
Greg Finley <gregory.finley@gmail.com>
2023-02-11 23:17:35 -0800

    Rework to a function

--- main.py
+++ main.py
@@ -2,19 +2,6 @@ import csv
 import os
 import json
 
-os.system(
-    "curl https://database.lichess.org/racingKings/lichess_db_racingKings_rated_2023-01.pgn.zst -o lichess_db_racingKings_rated_2023-01.pgn.zst"
-)
-
-os.system("pzstd -d lichess_db_racingKings_rated_2023-01.pgn.zst")
-
-os.system("rm lichess_db_racingKings_rated_2023-01.pgn.zst")
-
-file = "lichess_db_racingKings_rated_2023-01.pgn"
-
-num_games = 0
-keys = {}
-
 
 def parse_game(game_str):
     moves = []
@@ -44,56 +31,77 @@ def parse_game(game_str):
     return moves
 
 
-with open("games.json", "w") as games_json_file:
-    with open("moves.csv", "w") as moves_csv_file:
-        csv_writer = csv.writer(moves_csv_file, delimiter=",")
-        # csv_writer.writerow(["ply", "move", "clock", "eval", "game_id"])
-        game_id = ""
-        game = {}
-        with open(file) as f:
-            for line in f:
-                if line.startswith("["):
-                    if line.startswith("[Site"):
-                        game_id = (
-                            line.split(" ")[1]
-                            .removeprefix('"https://lichess.org/')
-                            .removesuffix('"]\n')
+def process_file(variant: str, year_month: str):
+    os.system(
+        f"curl https://database.lichess.org/{variant}/lichess_db_{variant}_rated_{year_month}.pgn.zst -o lichess_db_{variant}_rated_{year_month}.pgn.zst"
+    )
+
+    os.system(f"pzstd -d lichess_db_{variant}_rated_{year_month}.pgn.zst")
+
+    os.system(f"rm lichess_db_{variant}_rated_{year_month}.pgn.zst")
+
+    pgn_filename = f"lichess_db_{variant}_rated_{year_month}.pgn"
+    games_json_filename = f"games_{variant}_{year_month}.json"
+    moves_csv_filename = f"moves_{variant}_{year_month}.csv"
+
+    num_games = 0
+    keys = {}
+
+    with open(games_json_filename, "w") as games_json_file:
+        with open(moves_csv_filename, "w") as moves_csv_file:
+            csv_writer = csv.writer(moves_csv_file, delimiter=",")
+            # csv_writer.writerow(["ply", "move", "clock", "eval", "game_id"])
+            game_id = ""
+            game = {}
+            with open(pgn_filename) as pgn_file:
+                for line in pgn_file:
+                    if line.startswith("["):
+                        if line.startswith("[Site"):
+                            game_id = (
+                                line.split(" ")[1]
+                                .removeprefix('"https://lichess.org/')
+                                .removesuffix('"]\n')
+                            )
+                        # line is [Event "Rated Racing Kings game"]
+                        # key is Event
+                        # value is Rated Racing Kings game
+                        key = line.split(" ")[0].removeprefix("[")
+                        value = (
+                            line.removeprefix(f"[{key} ")
+                            .removeprefix(
+                                '"'
+                            )  # Seems like always a quote here, but split into two removeprefixes just in case
+                            .removesuffix("]\n")
+                            .removesuffix('"')  # Ditto
                         )
-                    # line is [Event "Rated Racing Kings game"]
-                    # key is Event
-                    # value is Rated Racing Kings game
-                    key = line.split(" ")[0].removeprefix("[")
-                    value = (
-                        line.removeprefix(f"[{key} ")
-                        .removeprefix(
-                            '"'
-                        )  # Seems like always a quote here, but split into two removeprefixes just in case
-                        .removesuffix("]\n")
-                        .removesuffix('"')  # Ditto
-                    )
-                    game[key] = value
+                        game[key] = value
 
-                    if key in keys:
-                        keys[key] += 1
-                    else:
-                        keys[key] = 1
+                        if key in keys:
+                            keys[key] += 1
+                        else:
+                            keys[key] = 1
 
-                if line.startswith("1. "):
-                    [csv_writer.writerow(move + [game_id]) for move in parse_game(line)]
-                    games_json_file.write(json.dumps(game) + "\n")
+                    if line.startswith("1. "):
+                        [
+                            csv_writer.writerow(move + [game_id])
+                            for move in parse_game(line)
+                        ]
+                        games_json_file.write(json.dumps(game) + "\n")
 
-                    num_games += 1
+                        num_games += 1
+
+    print("Num games", num_games)
+    print("Key count", keys)
+
+    # This will append if the table already exists
+    os.system(
+        f"bq load lichess.moves_{variant}_{year_month.replace('-', '_')} {moves_csv_filename} ply:integer,move:string,clock:string,eval:string,game_id:string"
+    )
+    os.system(
+        f"bq load --source_format=NEWLINE_DELIMITED_JSON --autodetect lichess.games_{variant}_{year_month.replace('-', '_')} {games_json_filename}"
+    )
+
+    os.system(f"rm {pgn_filename} {moves_csv_filename} {games_json_filename}")
 
 
-print("Num games", num_games)
-print("Key count", keys)
-
-# This will append if the table already exists
-os.system(
-    "bq load lichess.moves_python moves.csv ply:integer,move:string,clock:string,eval:string,game_id:string"
-)
-os.system(
-    "bq load --source_format=NEWLINE_DELIMITED_JSON --autodetect lichess.games_python games.json"
-)
-
-os.system("rm lichess_db_racingKings_rated_2023-01.pgn moves.csv games.json")
+process_file("racingKings", "2023-01")
