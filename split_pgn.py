1a9721d323598ec31263951d05a8eb0bb7865783
Greg Finley <gregory.finley@gmail.com>
2023-02-15 13:18:47 -0800

    Generalize split_pgn

--- split_pgn.py
+++ split_pgn.py
@@ -1,6 +1,22 @@
-mth = "2014-10"
+import os
 
-source_file = f"lichess_db_threeCheck_rated_{mth}.pgn"
+year_month = "2014-10"
+variant = "threeCheck"
+
+
+def os_run(command: str):
+    """Run os commands but stop if error"""
+    if os.system(command) != 0:
+        raise Exception(f"Error with {command}")
+
+
+os_run(
+    f"curl https://database.lichess.org/{variant}/lichess_db_{variant}_rated_{year_month}.pgn.zst --output lichess_db_{variant}_rated_{year_month}.pgn.zst"
+)
+
+os_run(f"pzstd -d lichess_db_{variant}_rated_{year_month}.pgn.zst")
+
+os_run(f"rm lichess_db_{variant}_rated_{year_month}.pgn.zst")
 
 # Write to smaller files in 2000-game chunks, the first file being
 # lichess_db_threeCheck_rated_2014-08_0001.pgn
@@ -8,10 +24,12 @@ source_file = f"lichess_db_threeCheck_rated_{mth}.pgn"
 num_games = 0
 games_per_file = 2000
 current_file_index = 1
-current_file_name = f"lichess_db_threeCheck_rated_{mth}_{current_file_index:04d}.pgn"
+current_file_name = (
+    f"lichess_db_{variant}_rated_{year_month}_{current_file_index:04d}.pgn"
+)
 current_file = open(current_file_name, "w")
 
-with open(source_file) as g:
+with open(f"lichess_db_{variant}_rated_{year_month}.pgn") as g:
     for line in g:
         current_file.write(line)
         if line.startswith("1. "):
