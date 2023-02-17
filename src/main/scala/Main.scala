ae345a6b79e7b2f964d042e852f96ec69e191ef5
Greg Finley <gregory.finley@gmail.com>
2023-02-16 23:05:18 -0800

    Fix parsing

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -2,29 +2,24 @@ import chess.format.pgn.{Parser, ParsedPgn, PgnStr}
 import scala.collection.mutable.ListBuffer
 import cats.data.Validated
 
-
 @main def parsePgn: Unit = 
-      val source = scala.io.Source.fromFile("lichess_db_racingKings_rated_2023-01.pgn")
-      for (line <- source.getLines()) {
-        var lines = new ListBuffer[String]()
-        lines += line
-        if (line.startsWith("1.")) {
-          //println("Found a game")
-          val pgn = PgnStr(line)
-          val parsedPgn = Parser.full(pgn)
-          parsedPgn match
-            case Validated.Invalid(errors) =>
-              println(s"Failed to parse PGN: ${errors.toString()}")
-              // halt the program
-              sys.exit(1)
-            case Validated.Valid(parsedPgn) =>
-              // parsedPgn.sans.value.foreach(x => println(x.metas))
-              println(parsedPgn.sans)
-              println(parsedPgn.tags)
-          // println(parsedPgn)
-          lines = new ListBuffer[String]()
-        }
-      }
-
-  // val parsedPgn = Parser.full(pgn)
-  // println(parsedPgn)
+  val source = scala.io.Source.fromFile("lichess_db_racingKings_rated_2023-01.pgn")
+  var lines = new ListBuffer[String]()
+  for (line <- source.getLines()) {
+    lines += line
+    if (line.startsWith("1.")) {
+      //println("Found a game")
+      val pgn = PgnStr(lines.mkString("\n"))
+      val parsedPgn = Parser.full(pgn)
+      parsedPgn match
+        case Validated.Invalid(errors) =>
+          println(s"Failed to parse PGN: ${errors.toString()}")
+          // halt the program
+          sys.exit(1)
+        case Validated.Valid(parsedPgn) =>
+          // parsedPgn.sans.value.foreach(x => println(x.metas))
+          println(parsedPgn.sans)
+          println(parsedPgn.tags)
+      lines = new ListBuffer[String]()
+    }
+  }
