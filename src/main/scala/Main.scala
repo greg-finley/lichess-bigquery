f1dc18f8f161f7a5218bc5a117fb3b629cb920a8
Greg Finley <gregory.finley@gmail.com>
2023-02-20 21:15:27 -0800

    Try to use Futures

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -9,12 +9,15 @@ import java.time.LocalDateTime
 import java.io._
 
 import scala.collection.mutable.{LinkedHashMap, ListBuffer}
-
 import scala.util.control.Breaks.*
+import scala.concurrent.Future
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.Await
+import scala.concurrent.duration.Duration
 
 // Freeze the list and ignore any future tags, so BigQuery has a consistent schema
 // Maybe if we get a new tag in the future we can edit the old schemas
-val gameTagValues: LinkedHashMap[String, String] = LinkedHashMap(
+val allTagValues: LinkedHashMap[String, String] = LinkedHashMap(
   "Event" -> null,
   "Site" -> null,
   "GameId" -> null,
@@ -49,7 +52,7 @@ val gameTagValues: LinkedHashMap[String, String] = LinkedHashMap(
 
 @main def parsePgn: Unit =
   val source =
-    scala.io.Source.fromFile("lichess_db_racingKings_rated_2023-01_0001.pgn")
+    scala.io.Source.fromFile("lichess_db_crazyhouse_rated_2023-01.pgn")
   val lines: ListBuffer[String] = ListBuffer()
   val gamesFile = new File("games.csv")
   val movesFile = new File("moves.csv")
@@ -57,105 +60,18 @@ val gameTagValues: LinkedHashMap[String, String] = LinkedHashMap(
   if (movesFile.exists()) movesFile.delete()
   val gameWriter = new PrintWriter(new FileWriter(gamesFile, true))
   val moveWriter = new PrintWriter(new FileWriter(movesFile, true))
-  var gameCount = 0
+  val futures = ListBuffer[Future[Unit]]()
   for (line <- source.getLines()) {
     lines += line
-    // tuple of (pgn move, clock, eval)
-    if (line.startsWith("[")) then
-      // [Site "https://lichess.org/CyEpEADM"]
-      val tag = line.split(" ")
-      val tagName = tag(0).replace("[", "")
-      if (gameTagValues.contains(tagName)) then
-        gameTagValues(tagName) = line
-          .substring(tagName.length() + 2)
-          .replace("]", "")
-          .replaceAll("\"", "")
-    else if (line.startsWith("1.")) then
-      val customMoveParsing = customParseMoves(line)
-      gameTagValues("GameId") = gameTagValues("Site").split("/")(3)
-      gameWriter.println(
-        gameTagValues.toSeq
-          .map { case (_, value) =>
-            Option(value).getOrElse("")
-          }
-          .foldLeft("") { (acc, value) =>
-            if (acc.isEmpty) value else acc + "," + value
-          }
-      )
-
-      gameCount += 1
-      val pgn = PgnStr(lines.mkString("\n"))
-
-      // tuple of (ply number, uci move, fen)
-      val lichessMoveParsing: List[(Int, String, String, String)] = Reader
-        .full(pgn)
-        .fold(
-          errors => {
-            println(s"Failed to read: ${errors.toString()}")
-            // halt the program
-            sys.exit(1)
-          },
-          result => {
-            result.valid.fold(
-              errors => {
-                println(s"Failed to parse PGN: ${errors.toString()}")
-                // halt the program
-                sys.exit(1)
-              },
-              replay => {
-                replay.chronoMoves.zipWithIndex.map((x, index) =>
-                  x.fold(
-                    { move =>
-                      (
-                        index + 1,
-                        move.toUci.uci,
-                        Fen
-                          .write(
-                            Situation.AndFullMoveNumber(
-                              move.situationAfter,
-                              Ply(index + 1).fullMoveNumber
-                            )
-                          )
-                          .toString(),
-                        gameTagValues("GameId")
-                      )
-                    },
-                    { drop =>
-                      (
-                        index + 1,
-                        drop.toUci.uci,
-                        Fen
-                          .write(
-                            Situation.AndFullMoveNumber(
-                              drop.situationAfter,
-                              Ply(index + 1).fullMoveNumber
-                            )
-                          )
-                          .toString(),
-                        gameTagValues("GameId")
-                      )
-                    }
-                  )
-                )
-              }
-            )
-          }
-        )
-      // zip together custom and lichess move parsing
-      customMoveParsing
-        .zip(lichessMoveParsing)
-        .foreach((x, y) =>
-          moveWriter.println(
-            (x._1, x._2, x._3, y._1, y._2, y._3, y._4).productIterator
-              .mkString(",")
-          )
-        )
-      lines.clear()
-      gameTagValues.foreach((x, y) => gameTagValues(x) = null)
-      if (gameCount % 500 == 0) {
-        println(LocalDateTime.now())
-        println(gameCount)
+    if (line.startsWith("1.")) then
+      futures += Future {
+        processGame(lines.toList, line, gameWriter, moveWriter)
       }
+      lines.clear()
+  }
+
+  for (future <- futures) {
+    Await.result(future, Duration.Inf)
   }
 
   gameWriter.close()
@@ -216,4 +132,104 @@ def customParseMoves(game_str: String): List[(String, String, String)] = {
     }
   }
   return moves
+}
+
+def processGame(
+    headerLines: List[String],
+    moveLine: String,
+    gameWriter: PrintWriter,
+    moveWriter: PrintWriter
+): Unit = {
+  val gameTagValues = allTagValues.clone()
+  for (line <- headerLines) {
+    if (line.startsWith("[")) {
+      val tag = line.split(" ")
+      val tagName = tag(0).replace("[", "")
+      if (gameTagValues.contains(tagName)) then
+        gameTagValues(tagName) = line
+          .substring(tagName.length() + 2)
+          .replace("]", "")
+          .replaceAll("\"", "")
+    }
+  }
+  val customMoveParsing = customParseMoves(moveLine)
+  gameTagValues("GameId") = gameTagValues("Site").split("/")(3)
+  gameWriter.println(
+    gameTagValues.toSeq
+      .map { case (_, value) =>
+        Option(value).getOrElse("")
+      }
+      .foldLeft("") { (acc, value) =>
+        if (acc.isEmpty) value else acc + "," + value
+      }
+  )
+
+  val allLines = headerLines :+ moveLine
+  val pgn = PgnStr(allLines.mkString("\n"))
+  println(pgn)
+
+  // tuple of (ply number, uci move, fen)
+  val lichessMoveParsing: List[(Int, String, String, String)] = Reader
+    .full(pgn)
+    .fold(
+      errors => {
+        println(s"Failed to read: ${errors.toString()}")
+        // halt the program
+        sys.exit(1)
+      },
+      result => {
+        result.valid.fold(
+          errors => {
+            println(s"Failed to parse PGN: ${errors.toString()}")
+            // halt the program
+            sys.exit(1)
+          },
+          replay => {
+            replay.chronoMoves.zipWithIndex.map((x, index) =>
+              x.fold(
+                { move =>
+                  (
+                    index + 1,
+                    move.toUci.uci,
+                    Fen
+                      .write(
+                        Situation.AndFullMoveNumber(
+                          move.situationAfter,
+                          Ply(index + 1).fullMoveNumber
+                        )
+                      )
+                      .toString(),
+                    gameTagValues("GameId")
+                  )
+                },
+                { drop =>
+                  (
+                    index + 1,
+                    drop.toUci.uci,
+                    Fen
+                      .write(
+                        Situation.AndFullMoveNumber(
+                          drop.situationAfter,
+                          Ply(index + 1).fullMoveNumber
+                        )
+                      )
+                      .toString(),
+                    gameTagValues("GameId")
+                  )
+                }
+              )
+            )
+          }
+        )
+      }
+    )
+  // zip together custom and lichess move parsing
+  customMoveParsing
+    .zip(lichessMoveParsing)
+    .foreach((x, y) =>
+      moveWriter.println(
+        (x._1, x._2, x._3, y._1, y._2, y._3, y._4).productIterator
+          .mkString(",")
+      )
+    )
 }
