a89d54985168b97838f5ac40909c110a806b88ee
Greg Finley <gregory.finley@gmail.com>
2023-02-23 14:17:54 -0800

    get one message at a time

--- src/main/scala/PubSubSubscriber.scala
+++ src/main/scala/PubSubSubscriber.scala
@@ -1,11 +1,20 @@
 package PgnParser
 
-import com.google.cloud.pubsub.v1.Subscriber
+import com.google.cloud.pubsub.v1.{MessageReceiver, Subscriber}
 import com.google.pubsub.v1.ProjectSubscriptionName
-import com.google.cloud.pubsub.v1.MessageReceiver
+import com.google.api.gax.batching.FlowControlSettings;
+
 object PubSubSubscriber {
   def getSubscriber(messageReceiver: MessageReceiver) =
     val subscriptionName =
       ProjectSubscriptionName.of("greg-finley", "lichess-file-sub1")
-    Subscriber.newBuilder(subscriptionName, messageReceiver).build()
+    Subscriber
+      .newBuilder(subscriptionName, messageReceiver)
+      .setFlowControlSettings(
+        FlowControlSettings
+          .newBuilder()
+          .setMaxOutstandingElementCount(1)
+          .build()
+      )
+      .build()
 }
